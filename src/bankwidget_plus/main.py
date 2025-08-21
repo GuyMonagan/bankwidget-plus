e6d4b30877f03736f2c5ccb60516c2cccbb10a36
GuyMonagan <werevalkury@gmail.com>
2025-08-21 14:40:12 +0400

    feat: реализация функций для главной страницы

--- src/bankwidget_plus/main.py
+++ src/bankwidget_plus/main.py
@@ -1,9 +1,12 @@
 from bankwidget_plus.data_loader import load_data
+from bankwidget_plus.views import homepage_view
+from bankwidget_plus.config import EXCEL_PATH
 
 def main():
-    print("Hello, курсовая!")
-    df = load_data("data/operations.xlsx")
-    print(df.head())
+    df = load_data(EXCEL_PATH)
+    date_input = input("Введите дату в формате YYYY-MM-DD HH:MM:SS: ")
+    json_result = homepage_view(df, date_input)
+    print(json_result)
 
 if __name__ == "__main__":
     main()
--- src/bankwidget_plus/utils.py
+++ src/bankwidget_plus/utils.py
@@ -1,3 +1,6 @@
-# utils.py
-def placeholder():
-    pass
+import pandas as pd
+from datetime import datetime
+
+def filter_transactions_since_date(df: pd.DataFrame, date: datetime) -> pd.DataFrame:
+    df["Дата операции"] = pd.to_datetime(df["Дата операции"], errors="coerce")
+    return df[df["Дата операции"] >= date].copy()
